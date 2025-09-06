cadb0835b5837bfdeb7147a0e223ac7fe397c4c8
TetaCindy <c.teta@alustudent.com>
2025-09-06 02:49:15 -0700

    0

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+# 0-simply_match_school.rb
+
+# Get the argument
+str = ARGV[0]
+
+# Match all occurrences of 'Holberton' and print them concatenated
+puts str.scan(/Holberton/).join
